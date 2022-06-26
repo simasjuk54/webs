7790d165eeea4bd2993fa5203d7bf5e83f553036
simasjuk54 <107644599+simasjuk54@users.noreply.github.com>
2022-06-26 18:02:25 +0300

    Add files via upload

---
+++ tests/TestCase.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests;
+
+use App\Services\Auth\Auth;
+use Doctrine\ORM\EntityManagerInterface;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    protected function transaction(): void
+    {
+        $this->app->make(EntityManagerInterface::class)->beginTransaction();
+    }
+
+    protected function rollback(): void
+    {
+        $this->app->make(EntityManagerInterface::class)->rollback();
+    }
+
+    protected function authAdmin(): void
+    {
+        $this->app->make(Auth::class)->authenticate('admin', 'admin');
+    }
+}
